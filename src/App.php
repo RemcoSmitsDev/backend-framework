2258710e3f03ba4251adedee62fe68b3590a2f9f
Remco Smits <remco@hjmediagroep.nl>
2021-11-06 14:15:38 +0100

    added new version of app class to make it more dynamic

--- src/App.php
+++ src/App.php
@@ -8,48 +8,32 @@ use Framework\Http\Api;
 
 class App
 {
-    public function start(Api $api, Content $content)
+    public function start()
     {
         // set timezone
         date_default_timezone_set('Europe/Amsterdam');
         setlocale(LC_ALL, 'nl_NL');
 
-        // api prefix
-        define('API_PREFIX', '/api');
-        // server root for constant vars
-        define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT']);
-
-        // check if app is [local|state(dev)|live]
+        // check if app is in development mode
         $this->checkAppState();
 
         // get error based on config option debug mode
         $this->catchErrors();
 
         // set Security headers
-        $this->setSecurityHeaders()->setSession();
+        $this->setSecurityHeaders();
 
-        // include all routes
-        require_once(realpath('../routes.php'));
-
-        // start listen for jquery ajax request
-        // api route: /api/PATH_TO_FILE_INSIDE_API_FILE
-        // then requires the correct file if request is valid
-        $api->listen();
-
-        // init all routes
-        route()->init();
-
-        // als er geen url match is dan wordt de 404 pagina weergegeven
-        $content->listen();
+        // set session settings
+        $this->setSession();
 
         // kijk of een van de tokens niet bestaat generate dan een token
         // om later ajax request te kunnen valideren
         if (!isset($_COOKIE['requestToken'],$_SESSION['requestToken'])) {
-            $api->generateRequestToken();
+            Api::generateRequestToken();
         }
     }
 
-    private function setSecurityHeaders(): self
+    private function setSecurityHeaders(): void
     {
         // block all iframes
         header('X-Frame-Options:deny');
@@ -59,16 +43,14 @@ class App
         header('X-Content-Type-Options: nosniff');
         // enable content strict Security
         header('Strict-Transport-Security: max-age=31536000');
-
-        return $this;
     }
 
-    private function setSession(): self
+    private function setSession(): void
     {
         // voeg http only session.cookie toe als de app niet op development mode staat
-        ini_set('session.cookie_httponly', !\Config::DEVELOPMENT_MODE);
+        ini_set('session.cookie_httponly', !IS_DEVELOPMENT_MODE);
         // voeg secure session.cookie toe als de app niet op development mode staat
-        ini_set('session.cookie_secure', !\Config::DEVELOPMENT_MODE);
+        ini_set('session.cookie_secure', !IS_DEVELOPMENT_MODE);
         // set session cookie duration time (2 days)
         session_set_cookie_params(3600 * 24 * 2);
 
@@ -76,14 +58,12 @@ class App
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
-
-        return $this;
     }
 
-    private function catchErrors()
+    private function catchErrors(): void
     {
         // shows errors when debug mode is on
-        if (\Config::DEBUG_MODE) {
+        if (IS_DEVELOPMENT_MODE) {
             ini_set('display_errors', 1);
             ini_set('display_startup_errors', 1);
             error_reporting(E_ALL);
@@ -98,7 +78,7 @@ class App
         }
     }
 
-    private function checkAppState(): self
+    private function checkAppState(): void
     {
         // define localhost ips to check if is development
         $whitelistLocalIps = [
@@ -112,6 +92,7 @@ class App
         // define host om in de config class te kunnen gebruiken
         define('HTTP_HOST', $_SERVER['HTTP_HOST']);
 
-        return $this;
+        // server root for constant vars
+        define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT']);
     }
 }
