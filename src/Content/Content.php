108ee218ffb55522dc0b222d9e362c629b8d06d3
Remco Smits <remco@hjmediagroep.nl>
2021-12-20 10:30:38 +0100

    fixed bug with content default template | fixed bug with route uri for home `/`

--- src/Content/Content.php
+++ src/Content/Content.php
@@ -4,153 +4,153 @@ namespace Framework\Content;
 
 class Content
 {
-    /**
-     * @var string $viewPath relative path to all views
-     */
-    private string $viewPath;
+	/**
+	 * @var string $viewPath relative path to all views
+	 */
+	private string $viewPath;
 
-    /**
-     * @var string $template Keeps track of all
-     */
-    private string $template = '404';
+	/**
+	 * @var string $template Keeps track of all
+	 */
+	private string $template = '404';
 
-    /**
-     * @var string|false $layout Keeps track of layout(head, global structure, footer)
-     */
-    private string|false $layout = '';
+	/**
+	 * @var string|false $layout Keeps track of layout(head, global structure, footer)
+	 */
+	private string|false $layout = '';
 
-    /**
-     * @var string|false $defaultLayout Keeps track of an default template
-     */
-    private string|false $defaultLayout = false;
+	/**
+	 * @var string|false $defaultLayout Keeps track of an default template
+	 */
+	private string|false $defaultLayout = false;
 
-    /**
-     * @var string $title Keeps track of page title
-     */
-    private string $title = '';
+	/**
+	 * @var string $title Keeps track of page title
+	 */
+	private string $title = '';
 
-    /**
-     * @var array Keeps track of all data to extract for all views
-     */
-    private array $data = [];
+	/**
+	 * @var array Keeps track of all data to extract for all views
+	 */
+	private array $data = [];
 
-    /**
-     * @param string|null $viewPath
-     * @param string|false $defaultLayout
-     */
-    public function __construct(?string $viewPath = null, string|false $defaultLayout = false)
-    {
-        $this->viewPath = rtrim(
-            $viewPath ?: SERVER_ROOT . '/../templates',
-            '/'
-        ) . '/';
-        $this->defaultLayout = $defaultLayout;
-    }
+	/**
+	 * @param string|null $viewPath
+	 * @param string|false $defaultLayout
+	 */
+	public function __construct(?string $viewPath = null, string|false $defaultLayout = false)
+	{
+		$this->viewPath = rtrim(
+			$viewPath ?: SERVER_ROOT . '/../templates',
+			'/'
+		) . '/';
+		$this->defaultLayout = $defaultLayout;
+	}
 
-    /**
-     * This function sets the main template that will be rendered within the layout file with:
-     * content()->renderTemplate();
-     * @param string $template
-     * @param array $data
-     * @return self
-     */
-    public function template(string $template, array $data = []): self
-    {
-        // merge old data into deeper view
-        $this->data = array_merge($this->data, $data);
-        // set template as content wrapper
-        $this->template = $template;
+	/**
+	 * This function sets the main template that will be rendered within the layout file with:
+	 * content()->renderTemplate();
+	 * @param string $template
+	 * @param array $data
+	 * @return self
+	 */
+	public function template(string $template, array $data = []): self
+	{
+		// merge old data into deeper view
+		$this->data = array_merge($this->data, $data);
+		// set template as content wrapper
+		$this->template = $template;
 
-        // return self
-        return $this;
-    }
+		// return self
+		return $this;
+	}
 
-    /**
-     * This function sets the layout type, this wil make an file path like: include{type}Content.php
-     * @param string|false $layout
-     * @return Content
-     */
-    public function layout(string|false $layout): self
-    {
-        // set layout
-        $this->layout = $layout;
+	/**
+	 * This function sets the layout type, this wil make an file path like: include{type}Content.php
+	 * @param string|false $layout
+	 * @return Content
+	 */
+	public function layout(string|false $layout): self
+	{
+		// set layout
+		$this->layout = $layout;
 
-        // return self
-        return $this;
-    }
+		// return self
+		return $this;
+	}
 
-    /**
-     * This function renders an view/file with data extracted
-     * @param string $view
-     * @param array $data
-     * @return self
-     */
-    public function view(string $view, array $data = []): self
-    {
-        // merge old data into deeper view
-        $this->data = array_merge($this->data, $data);
+	/**
+	 * This function renders an view/file with data extracted
+	 * @param string $view
+	 * @param array $data
+	 * @return self
+	 */
+	public function view(string $view, array $data = []): self
+	{
+		// merge old data into deeper view
+		$this->data = array_merge($this->data, $data);
 
-        // kijk of een template part bestaat
-        if (file_exists($view = $this->viewPath . $view . '.php')) {
-            // compact array
-            extract($this->data);
+		// kijk of een template part bestaat
+		if (file_exists($view = $this->viewPath . $view . '.php')) {
+			// compact array
+			extract($this->data);
 
-            // require file
-            require($view);
-        }
+			// require file
+			require($view);
+		}
 
-        // return self
-        return $this;
-    }
+		// return self
+		return $this;
+	}
 
-    /**
-     * This function sets the content title, this needs to be done when you set the template
-     * @param string $title
-     * @return self
-     */
-    public function title(string $title): self
-    {
-        // set title 
-        $this->title = $title;
+	/**
+	 * This function sets the content title, this needs to be done when you set the template
+	 * @param string $title
+	 * @return self
+	 */
+	public function title(string $title): self
+	{
+		// set title 
+		$this->title = $title;
 
-        // return self
-        return $this;
-    }
+		// return self
+		return $this;
+	}
 
-    /**
-     * Get content title
-     * @return string
-     */
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
+	/**
+	 * Get content title
+	 * @return string
+	 */
+	public function getTitle(): string
+	{
+		return $this->title;
+	}
 
-    /**
-     * Listen if there is an template set
-     * @return void
-     */
-    public function listen(): void
-    {
-        if ($this->layout !== false) {
-            $this->layout($this->defaultLayout);
-        }
-        // check if content wrapper exists
-        if (file_exists($path = $this->viewPath . "{$this->layout}.php")) {
-            // require wrapper template
-            require_once($path);
-        }
-    }
+	/**
+	 * Listen if there is an template set
+	 * @return void
+	 */
+	public function listen(): void
+	{
+		if ($this->layout !== false && $this->layout === '' && $this->defaultLayout) {
+			$this->layout($this->defaultLayout);
+		}
+		// check if content wrapper exists
+		if (file_exists($path = $this->viewPath . "{$this->layout}.php")) {
+			// require wrapper template
+			require_once($path);
+		}
+	}
 
-    /**
-     * This renders the given template with extracted data
-     * @return void
-     */
-    public function renderTemplate(): void
-    {
-        // view main template
-        $this->view($this->template);
-        // reset template
-        $this->template = '';
-    }
+	/**
+	 * This renders the given template with extracted data
+	 * @return void
+	 */
+	public function renderTemplate(): void
+	{
+		// view main template
+		$this->view($this->template);
+		// reset template
+		$this->template = '';
+	}
 }
--- src/Http/Request.php
+++ src/Http/Request.php
@@ -6,251 +6,251 @@ use ReflectionException;
 
 class Request extends RequestValidator
 {
-    /**
-     * @var string
-     */
-    private string $method;
+	/**
+	 * @var string
+	 */
+	private string $method;
 
-    /**
-     * @var object|null
-     */
-    private ?object $getData;
+	/**
+	 * @var object|null
+	 */
+	private ?object $getData;
 
-    /**
-     * @var object|null
-     */
-    private ?object $postData;
+	/**
+	 * @var object|null
+	 */
+	private ?object $postData;
 
-    /**
-     * @var object|null
-     */
-    private ?object $fileData;
+	/**
+	 * @var object|null
+	 */
+	private ?object $fileData;
 
-    /**
-     * @var object
-     */
-    public object $requestData;
+	/**
+	 * @var object
+	 */
+	public object $requestData;
 
 
-    public function __construct()
-    {
-        // set request method
-        $this->method = $this->method();
+	public function __construct()
+	{
+		// set request method
+		$this->method = $this->method();
 
-        // set all request(get) data
-        $this->getData = (object) clearInjections($_GET);
+		// set all request(get) data
+		$this->getData = (object) clearInjections($_GET);
 
-        // set all request(post) data
-        $this->postData = (object) clearInjections(array_merge(
-            json_decode(file_get_contents('php://input'), true) ?? [],
-            $_POST
-        ));
+		// set all request(post) data
+		$this->postData = (object) clearInjections(array_merge(
+			json_decode(file_get_contents('php://input'), true) ?? [],
+			$_POST
+		));
 
-        // add all request files(upload)
-        $this->fileData = (object) $_FILES;
+		// add all request files(upload)
+		$this->fileData = (object) $_FILES;
 
-        // merge all request data
-        $this->requestData = (object) array_merge(
-            (array) $this->getData,
-            (array) $this->postData,
-            (array) $this->fileData
-        );
-    }
+		// merge all request data
+		$this->requestData = (object) array_merge(
+			(array) $this->getData,
+			(array) $this->postData,
+			(array) $this->fileData
+		);
+	}
 
-    /**
-     * This function will return value of class property is exists
-     * @param string $name
-     * @return mixed
-     */
-    public function __get(string $name): mixed
-    {
-        return property_exists($this->requestData, $name) ? $this->requestData->{$name} : null;
-    }
+	/**
+	 * This function will return value of class property is exists
+	 * @param string $name
+	 * @return mixed
+	 */
+	public function __get(string $name): mixed
+	{
+		return property_exists($this->requestData, $name) ? $this->requestData->{$name} : null;
+	}
 
-    /**
-     * This function will return all get data or the value by find key
-     * @param string|int|null $find
-     * @return mixed
-     */
+	/**
+	 * This function will return all get data or the value by find key
+	 * @param string|int|null $find
+	 * @return mixed
+	 */
 
-    public function get(string|int|null $find = null): mixed
-    {
-        // check if find is null
-        if (is_null($find)) {
-            return $this->getData;
-        }
+	public function get(string|int|null $find = null): mixed
+	{
+		// check if find is null
+		if (is_null($find)) {
+			return $this->getData;
+		}
 
-        // return get data
-        return property_exists($this->getData, $find) ? $this->getData->{$find} : null;
-    }
+		// return get data
+		return property_exists($this->getData, $find) ? $this->getData->{$find} : null;
+	}
 
-    /**
-     * This function will return all post data or the value by find key
-     * @param string|int|null $find
-     * @return mixed
-     */
-    public function post(string|int|null $find = null): mixed
-    {
-        // check if find is null
-        if (is_null($find)) {
-            return $this->postData;
-        }
+	/**
+	 * This function will return all post data or the value by find key
+	 * @param string|int|null $find
+	 * @return mixed
+	 */
+	public function post(string|int|null $find = null): mixed
+	{
+		// check if find is null
+		if (is_null($find)) {
+			return $this->postData;
+		}
 
-        // return get data based on find value
-        return property_exists($this->postData, $find) ? $this->postData->{$find} : null;
-    }
+		// return get data based on find value
+		return property_exists($this->postData, $find) ? $this->postData->{$find} : null;
+	}
 
-    /**
-     * This function will return all request file(s)
-     * @param string|null $find
-     */
-    public function file(?string $find = null): mixed
-    {
-        // check if find is null
-        if (is_null($find)) {
-            return $this->fileData;
-        }
+	/**
+	 * This function will return all request file(s)
+	 * @param string|null $find
+	 */
+	public function file(?string $find = null): mixed
+	{
+		// check if find is null
+		if (is_null($find)) {
+			return $this->fileData;
+		}
 
-        // return get data based on find value
-        return property_exists($this->fileData, $find) ? $this->fileData->{$find} : null;
-    }
+		// return get data based on find value
+		return property_exists($this->fileData, $find) ? $this->fileData->{$find} : null;
+	}
 
-    /**
-     * This function will return all request information
-     * @return ?object
-     */
-    public function all(): ?object
-    {
-        return $this->requestData;
-    }
+	/**
+	 * This function will return all request information
+	 * @return ?object
+	 */
+	public function all(): ?object
+	{
+		return $this->requestData;
+	}
 
-    /**
-     * This function will check if all keys exists in the current request
-     * @param string $requestKey
-     * @return bool
-     */
-    public function exists(string ...$requestKey): bool
-    {
-        // loop trough all func args
-        foreach ($requestKey as $key) {
-            // check if key exists
-            if (!property_exists($this->requestData, $key)) {
-                return false;
-            }
-        }
-        return true;
-    }
+	/**
+	 * This function will check if all keys exists in the current request
+	 * @param string $requestKey
+	 * @return bool
+	 */
+	public function exists(string ...$requestKey): bool
+	{
+		// loop trough all func args
+		foreach ($requestKey as $key) {
+			// check if key exists
+			if (!property_exists($this->requestData, $key)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    /**
-     * @return string
-     */
-    public function csrf(): string
-    {
-        return $_SESSION['_csrf_token'] ?? $_SESSION['_csrf_token'] = randomString();
-    }
+	/**
+	 * @return string
+	 */
+	public function csrf(): string
+	{
+		return $_SESSION['_csrf_token'] ?? $_SESSION['_csrf_token'] = randomString();
+	}
 
-    /**
-     * @return bool
-     * @throws ReflectionException
-     */
-    public function validateCsrf(): bool
-    {
-        return \request('_token') === $this->csrf();
-    }
+	/**
+	 * @return bool
+	 * @throws ReflectionException
+	 */
+	public function validateCsrf(): bool
+	{
+		return \request('_token') === $this->csrf();
+	}
 
-    /**
-     * This function will return current uri like: /route/to
-     * @return string
-     */
-    public function uri(): string
-    {
-        // krijg de huidige url zonden get waardes
-        return rtrim(
-            parse_url(rawurldecode($this->url()), PHP_URL_PATH),
-            '/'
-        );
-    }
+	/**
+	 * This function will return current uri like: /route/to
+	 * @return string
+	 */
+	public function uri(): string
+	{
+		// krijg de huidige url zonden get waardes
+		return rtrim(
+			parse_url(rawurldecode($this->url()), PHP_URL_PATH),
+			'/'
+		) ?: '/';
+	}
 
-    /**
-     * This function will return the query string
-     * @return string
-     */
-    public function query(): string
-    {
-        return parse_url(rawurldecode($this->url()), PHP_URL_QUERY) ?? '';
-    }
+	/**
+	 * This function will return the query string
+	 * @return string
+	 */
+	public function query(): string
+	{
+		return parse_url(rawurldecode($this->url()), PHP_URL_QUERY) ?? '';
+	}
 
-    /**
-     * This function will return current URL with params
-     * @return string
-     */
-    public function url(): string
-    {
-        // krijg de huidige url zonden get waardes
-        return rtrim($_SERVER['REQUEST_URI'], '/') ?: '/';
-    }
+	/**
+	 * This function will return current URL with params
+	 * @return string
+	 */
+	public function url(): string
+	{
+		// krijg de huidige url zonden get waardes
+		return rtrim($_SERVER['REQUEST_URI'], '/') ?: '/';
+	}
 
-    /**
-     * This function will return all headers of one header based on the findHeader param
-     * @param string|null $findHeader
-     * @return array|string|null
-     */
-    public function headers(?string $findHeader = null): array|string|null
-    {
-        // keep track of all headers
-        $headers = [];
+	/**
+	 * This function will return all headers of one header based on the findHeader param
+	 * @param string|null $findHeader
+	 * @return array|string|null
+	 */
+	public function headers(?string $findHeader = null): array|string|null
+	{
+		// keep track of all headers
+		$headers = [];
 
-        // If getallheaders() is available, use that
-        if (function_exists('getallheaders')) {
-            // get all headers
-            $headers = getallheaders();
+		// If getallheaders() is available, use that
+		if (function_exists('getallheaders')) {
+			// get all headers
+			$headers = getallheaders();
 
-            // getallheaders() can return false if something went wrong
-            if ($headers !== false) {
-                return $findHeader ? $headers[$findHeader] ?? null : $headers;
-            }
-        }
+			// getallheaders() can return false if something went wrong
+			if ($headers !== false) {
+				return $findHeader ? $headers[$findHeader] ?? null : $headers;
+			}
+		}
 
-        // Method getallheaders() not available or went wrong: manually extract 'm
-        foreach ($_SERVER as $name => $value) {
-            if ((str_starts_with($name, 'HTTP_')) || ($name == 'CONTENT_TYPE') || ($name == 'CONTENT_LENGTH')) {
-                $headers[str_replace(
-                    [' ', 'Http'],
-                    ['-', 'HTTP'],
-                    ucwords(strtolower(str_replace('_', ' ', substr($name, 5))))
-                )] = $value;
-            }
-        }
+		// Method getallheaders() not available or went wrong: manually extract 'm
+		foreach ($_SERVER as $name => $value) {
+			if ((str_starts_with($name, 'HTTP_')) || ($name == 'CONTENT_TYPE') || ($name == 'CONTENT_LENGTH')) {
+				$headers[str_replace(
+					[' ', 'Http'],
+					['-', 'HTTP'],
+					ucwords(strtolower(str_replace('_', ' ', substr($name, 5))))
+				)] = $value;
+			}
+		}
 
-        // return header(s) based on find header key
-        return $findHeader ? (array_key_exists($findHeader, $headers) ? $headers[$findHeader] : null) : $headers;
-    }
+		// return header(s) based on find header key
+		return $findHeader ? (array_key_exists($findHeader, $headers) ? $headers[$findHeader] : null) : $headers;
+	}
 
-    /**
-     * This function will return current request type
-     * @return string
-     */
-    public function method(): string
-    {
-        // Take the method as found in $_SERVER
-        $method = strtoupper($_SERVER['REQUEST_METHOD']);
+	/**
+	 * This function will return current request type
+	 * @return string
+	 */
+	public function method(): string
+	{
+		// Take the method as found in $_SERVER
+		$method = strtoupper($_SERVER['REQUEST_METHOD']);
 
-        // If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
-        // @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
-        if ($_SERVER['REQUEST_METHOD'] === 'HEAD') {
-            // start output buffer
-            ob_start();
-            // set method to get
-            $method = 'GET';
-        }
+		// If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
+		// @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
+		if ($_SERVER['REQUEST_METHOD'] === 'HEAD') {
+			// start output buffer
+			ob_start();
+			// set method to get
+			$method = 'GET';
+		}
 
-        // If it's a POST request, check for a method override header
-        elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && in_array(strtoupper($this->headers('X-HTTP-Method-Override') ?? ''), ['PUT', 'DELETE', 'PATCH'])) {
-            // check if headers exists
-            $method = $this->headers('X-HTTP-Method-Override');
-        }
+		// If it's a POST request, check for a method override header
+		elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && in_array(strtoupper($this->headers('X-HTTP-Method-Override') ?? ''), ['PUT', 'DELETE', 'PATCH'])) {
+			// check if headers exists
+			$method = $this->headers('X-HTTP-Method-Override');
+		}
 
-        // return method in strtoupper
-        return strtoupper($method);
-    }
+		// return method in strtoupper
+		return strtoupper($method);
+	}
 }
