ff1c95db7400b0badc9ac31f92a910e210046dc9
Remco Smits <djsmits12@gmail.com>
2022-02-11 19:12:30 +0100

    added test for `dependency injection Container`

--- tests/Container/ContainerTest.php
+++ tests/Container/ContainerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Framework\Tests\Container;
 
+use Closure;
 use Framework\Container\Container;
 use PHPUnit\Framework\TestCase;
 use InvalidArgumentException;
@@ -17,7 +18,7 @@ class ClosureTest
 	) {
 	}
 
-	public function test(string $name, $test = null): ClosureTest
+	public function test(string $name, ?int $age, $test = null): ClosureTest
 	{
 		return $this;
 	}
@@ -54,21 +55,23 @@ class ContainerTest extends TestCase
 		$this->assertEquals('test1', $response->name);
 
 		$closure = function (ClosureTest $closureTest, string $test) {
-			return $closureTest;
+			return func_get_args();
 		};
 
 		$response = Container::handleClosure($closure, ['name' => 'test1', 'test' => 'this is a test string']);
 
-		$this->assertInstanceOf(ClosureTest::class, $response);
-		$this->assertEquals('test1', $response->name);
+		$this->assertInstanceOf(ClosureTest::class, $response[0]);
+		$this->assertEquals('test1', $response[0]->name);
+		$this->assertEquals('this is a test string', $response[1]);
 
 		$closure = function (ClosureTest $closureTest, string $test = 'default') {
-			return $closureTest;
+			return func_get_args();
 		};
 
 		$response = Container::handleClosure($closure, ['name' => 'test1']);
 
-		$this->assertInstanceOf(ClosureTest::class, $response);
+		$this->assertInstanceOf(ClosureTest::class, $response[0]);
+		$this->assertEquals('default', $response[1]);
 	}
 
 	public function test_handle_private_function_missing_param()
@@ -76,9 +79,28 @@ class ContainerTest extends TestCase
 		$this->expectException(InvalidArgumentException::class);
 
 		$closure = function (ClosureTest $closureTest, string $test) {
-			return $closureTest;
+			return func_get_args();
 		};
 
-		Container::handleClosure($closure, ['name' => 'test1']);
+		$response = Container::handleClosure($closure, ['name' => 'test1']);
+
+		$this->assertInstanceOf(ClosureTest::class, $response[0]);
+	}
+
+	public function test_handle_private_function_missing_optional_extra_parameter()
+	{
+		$callback = function () {
+		};
+
+		$closure = function (Closure $callback, ?string $arg = null, ?int $type = null) {
+			return func_get_args();
+		};
+
+		// this will throws an error bc `type` will be at the `arg` place
+		$response = Container::HandleClosure($closure, ['callback' => $callback, 'type' => 10]);
+
+		$this->assertInstanceOf(Closure::class, $response[0]);
+		$this->assertEquals(null, $response[1]);
+		$this->assertEquals(10, $response[2]);
 	}
 }
